0c7b32b7f49a2eb7f0453f5f6d2cb4a8b496595a
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2023-12-10 18:31:16 +0200

    created container and some html elements, forms and table

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const container = createDiv('container') //create container
+const sectionLeft = createDiv('section-left') //create section left
+const sectionRight = createDiv('section-right') //create section right
 const subtitle = createTitle('h1', 'title', 'NEW STUDENT') //create title
 
-//func create div
+//func create tag div
 function createDiv(className) {
   const div = document.createElement('div')
   div.classList.add(className)
   return div
 }
 
-//func create title
+//func create tag title
 function createTitle(tag, className, text) {
   const title = document.createElement(tag)
   title.classList.add(className)
@@ -16,33 +18,30 @@ function createTitle(tag, className, text) {
   return title
 }
 
-//func create form
+//func create tag form
 function createForm(className, id) {
   let form = document.createElement('form')
-  form.id = id 
+  form.id = id
   form.classList.add(className)
   return form
 }
 
-//func create input
-function createInput(className, type, placeholder, id) {
+//func create tag input
+function createInput(className, id) {
   let input = document.createElement('input')
   input.classList.add(className)
-  input.type = type
-  input.placeholder = placeholder
   input.id = id
   return input
 }
 
-//func create label
-function createLabel(className, text) {
+//func create tag label
+function createLabel(text) {
   let label = document.createElement('label')
-  label.classList.add(className)
   label.textContent = text
   return label
 }
 
-//func create button
+//func create tag button
 function createButton(className, text, id) {
   let button = document.createElement('button')
   button.classList.add(className)
@@ -51,7 +50,7 @@ function createButton(className, text, id) {
   return button
 }
 
-//func create list 
+//func create tag ul 
 function createList(className, id) {
   const list = document.createElement('ul')
   list.classList.add(className)
@@ -59,24 +58,156 @@ function createList(className, id) {
   return list
 }
 
-//func create list-item
-function createListItem(className, id) {
-  const item = document.createElement('li')
-  item.classList.add(className)
-  item.id = id
+//func create form for adding new student
+function getFormAddNewStudent() {
+  const form = createForm('form', 'form') //create form
+  const button = createButton('form__btn', 'ADD STUDENT', 'form-btn') //create button
 
-  const btnName = createButton('list__item-btn-name', 'NAME', 'sorting-name')
-  const btnAge = createButton('list__item-btn-age', 'AGE', 'sorting-age')
-  const btnFaculty = createButton('list__item-btn-faculty', 'FACULTY', 'sorting-faculty')
-  const btnEducation = createButton('list__item-btn-education', 'EDUCATION', 'sorting-education')
+  const inputComponents = [ //arr of input details
+    {
+      type: 'text',
+      id: 'input-name',
+      placeholder: 'NAME',
+    },
+    {
+      type: 'text',
+      id: 'input-faculty',
+      placeholder: 'SURNAME',
+    },
+    {
+      type: 'date',
+      id: 'input-study-start',
+      placeholder: '',
+    },
+    {
+      type: 'text',
+      id: 'input-study-finish',
+      placeholder: 'FACULTY',
+    }, 
+    {
+      type: 'text',
+      id: 'input-study-finish',
+      placeholder: 'START STUDY(year)'
+    },
+  ]
 
-  const btnRemove = createButton('list__item-btn-remove', 'remove', 'btn-remove')
+  for (i = 0; i < 5; i++) { //add input and input box
+    const { type, id, placeholder } = inputComponents[i] //get data from array of objects
 
+    const inputBox = createDiv('form__input-box') //create input box
+    const input = createInput('form__input', id) //create input
 
-  item.append(btnName, btnAge, btnFaculty, btnEducation, btnRemove)
+    input.placeholder = placeholder //add placeholder
+    input.type = type //add type
 
-  return item
+    inputBox.append(input)
+    form.append(inputBox, button)
+  }
+
+  sectionLeft.append(form)
+
+  return form
 }
 
-container.append(subtitle)
+//func create filter by something
+function createFilter() {
+  const filterForm = createForm('nav', 'filter_form') //create filter form
+  const filterTitle = createTitle('h3', 'filter-form__title', 'FILTER BY') //create title of filter
+
+  const labels = ['name', 'faculty', 'start educ:', 'finish educ:'] //array of label text
+  const inputDetails = [ //arr of input details
+    {
+      type: 'text',
+      id: 'filter-name',
+      name: 'filtName'
+    },
+    {
+      type: 'text',
+      id: 'filter-faculty',
+      name: 'filtFaculty'
+    },
+    {
+      type: 'number',
+      id: 'filter-study-start',
+      name: 'filtStart'
+    },
+    {
+      type: 'number',
+      id: 'filter-study-finish',
+      name: 'filtFinish'
+    },
+  ]
+
+  filterForm.append(filterTitle) //add title to form
+
+  for (i = 0; i < inputDetails.length; i++) { //add label and input to form
+    const { type, id, name } = inputDetails[i] //get data from array of objects
+
+    const filterLabel = createLabel(labels[i])
+    filterLabel.setAttribute('for', id) //add for to connect with input
+
+    const filterInput = createInput('filter-form__btn', id)
+    filterInput.type = type //add type
+    filterInput.name = name //add name
+
+    filterForm.append(filterLabel, filterInput)
+  }
+
+  sectionRight.append(filterForm)
+
+  return filterForm
+}
+
+//func create table
+function createTable() {
+  const table = createList('table', 'table_id') //create table
+  const item = document.createElement('li') //create li
+  item.classList.add('table__sort__data') //add class name
+  item.id = 'table__sort__data' //add id
+
+  const btnDetails = [ //arr of btn details
+    {
+      text: 'NAME',
+      id: 'sorting-name',
+    },
+    {
+      text: 'AGE',
+      id: 'sorting-faculty',
+    },
+    {
+      text: 'FACULTY',
+      id: 'sorting-study-start',
+    },
+    {
+      text: 'EDUCATION',
+      id: 'sorting-study-finish',
+    },
+    {
+      text: 'remove',
+      id: 'sorting-remove',
+    },
+  ]
+
+  for(let i = 0; i < 5; i++) {
+    const {text, id} = btnDetails[i] //get data from array of objects
+
+    const tableButton = createButton('table__sorting-btn', text , id) //create li
+    tableButton.classList.add(`sort-btn${i + 1}`) //add new class
+
+    item.append(tableButton)
+  }
+
+  table.append(item)
+  sectionRight.append(table)
+
+  return table
+}
+
+sectionLeft.append(subtitle)
+
+getFormAddNewStudent() //call func get new student info
+createFilter() //call func create filter form
+createTable() //call func create table
+
+container.append(sectionLeft, sectionRight)
 document.body.append(container)
